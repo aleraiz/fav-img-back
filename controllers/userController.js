63d1a0a1ce9728b2a4366e581a612bde28299b02
aleraiz <alejandroraiz7@gmail.com>
2023-04-11 23:48:59 -0300

    cambios en userController(seeProfile)

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,10 +3,16 @@ const jwt = require("jsonwebtoken");
 
 const seeProfile = async (req, res) => {
   const { id } = req.params;
-  const user = await UserModel.findById(id);
-  console.log(id);
-  console.log(user);
-  return res.status(200).json(user);
+
+  try {
+    const user = await UserModel.findById(id);
+    console.log(id);
+    console.log(user);
+    return res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json(error);
+  }
 };
 
 const signUp = async (req, res) => {
